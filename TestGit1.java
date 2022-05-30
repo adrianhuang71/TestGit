ef3675bbd731bf239c90a523bf0b71fdf5aca8b3
Adrian <adrian@innoagile.com.tw>
2022-05-30 23:05:48 +0800

    TestGit1 and TestGit2 Update

--- TestGit1.java
+++ TestGit1.java
@@ -3,5 +3,6 @@ package TestGit;
 public class TestGit1 {
 
 	public static void main(String[] args) {
+			System.out.println("TestGit1");
 		}
 	}
--- TestGit2.java
+++ TestGit2.java
@@ -3,5 +3,6 @@ package TestGit;
 public class TestGit2 {
 
 	public static void main(String[] args) {
+		System.out.println("TestGit2");
 	}
 }
